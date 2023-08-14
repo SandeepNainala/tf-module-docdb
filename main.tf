7f569878de3712b53d9f41930c4176dfc29cf872
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-08-14 18:20:11 +0530

    Terraform - roboshop - application load balance - alb listener - target group assigned to auto scaling- listener - iam - target group(targets)

--- main.tf
+++ main.tf
@@ -2,32 +2,31 @@ resource "aws_docdb_subnet_group" "main" {
   name       = "${var.name}-${var.env}"
   subnet_ids = var.subnets
 
-  tags = merge(var.tags,{ Name = "${var.name}-${var.env}-sng"})
+  tags = merge(var.tags, { Name = "${var.name}-${var.env}-sng" })
 }
 
 resource "aws_security_group" "main" {
-  name    = "${var.name}-${var.env}-sg"
+  name        = "${var.name}-${var.env}-sg"
   description = "${var.name}-${var.env}-sg"
-  vpc_id = var.vpc_id
+  vpc_id      = var.vpc_id
 
   ingress {
     description = "DOCDB"
-    from_port = var.port_no
-    protocol  = "tcp"
-    to_port   = var.port_no
+    from_port   = var.port_no
+    to_port     = var.port_no
+    protocol    = "tcp"
     cidr_blocks = var.allow_db_cidr
   }
 
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  tags = merge(var.tags,{ Name = "${var.name}-${var.env}-sg"})
-
+  tags = merge(var.tags, { Name = "${var.name}-${var.env}-sg" })
 }
 
 resource "aws_docdb_cluster_parameter_group" "main" {
